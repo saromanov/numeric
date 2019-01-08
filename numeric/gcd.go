86bc04e82026b9eee71da6ba8c84b173cc617e8a
Sergey <xxsmotur@gmail.com>
2019-01-08 17:23:42 +0500

    Add testing of GCD

--- numeric/gcd.go
+++ numeric/gcd.go
@@ -3,6 +3,12 @@ package numeric
 // GCD provides calculation for greastest common divisor
 // of two numbers
 func GCD(x, y int64) int64 {
+	if x < 0 {
+		x = 0 - x
+	}
+	if y < 0 {
+		y = 0 - y
+	}
 	var c int64
 	for y > 0 {
 		c = x % y
---
+++ numeric/gcd_test.go
@@ -0,0 +1,20 @@
+package numeric
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGCD(t *testing.T) {
+	r := GCD(100, 24)
+	assert.Equal(t, r, int64(4), "numbers is not equal")
+	r = GCD(24, 100)
+	assert.Equal(t, r, int64(4), "numbers is not equal")
+	r = GCD(5, 24)
+	assert.Equal(t, r, int64(1), "numbers is not equal")
+	r = GCD(5, 10)
+	assert.Equal(t, r, int64(5), "numbers is not equal")
+	r = GCD(5, -10)
+	assert.Equal(t, r, int64(5), "numbers is not equal")
+}
