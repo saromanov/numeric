fdb41bd2da0e6ab7740944bba5848b3e66fb586d
Sergey <xxsmotur@gmail.com>
2019-01-09 15:15:20 +0500

    Add tests for dor product

--- linear/vector_test.go
+++ linear/vector_test.go
@@ -19,3 +19,36 @@ func TestLen(t *testing.T) {
 	v = Vector([]float64{5, 4, 7, 8, 5, 2, 3})
 	assert.Equal(t, v.Len(), 7, "length of vector is not equal")
 }
+
+func TestDot(t *testing.T) {
+	v := Vector([]float64{1, 2, 5})
+	p := Vector([]float64{2, 2, 2})
+	r, err := v.Dot(p)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, r, float64(16), "result should be equal")
+
+	v = Vector([]float64{10, -5})
+	p = Vector([]float64{20, 1})
+	r, err = v.Dot(p)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, r, float64(195), "result should be equal")
+
+	v = Vector([]float64{1, 2, 5, 8, 5, 10})
+	p = Vector([]float64{2, 2, 2, 3, 2, 5})
+	r, err = v.Dot(p)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, r, float64(100), "result should be equal")
+
+	v = Vector([]float64{1, 2, 5, 8, 5, 10})
+	p = Vector([]float64{2})
+	_, err = v.Dot(p)
+	if err == nil {
+		t.Fatal("vector mismatch")
+	}
+}
