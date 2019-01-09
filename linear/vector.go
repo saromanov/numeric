a958e6a1684ab0087478b3e6441a4f7fb6c4b99b
Sergey <xxsmotur@gmail.com>
2019-01-09 15:07:26 +0500

    Vector: add dor product

--- linear/vector.go
+++ linear/vector.go
@@ -1,5 +1,9 @@
 package linear
 
+import "errors"
+
+var errVectorMismatch = errors.New("vector mismatch")
+
 // Vector provides definition of the vector
 type Vector []float64
 
@@ -9,6 +13,14 @@ func (v Vector) Len() int {
 }
 
 // Dot provides implementation of dot product
-func (v Vector) Dot(p Vector) Vector {
-	return v
+func (v Vector) Dot(p Vector) (float64, error) {
+	if v.Len() != p.Len() {
+		return 0, errVectorMismatch
+	}
+
+	var result float64
+	for i := 0; i < v.Len(); i++ {
+		result += v[i] * p[i]
+	}
+	return result, nil
 }
