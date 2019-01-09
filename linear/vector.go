e5782ff1c0849b33a8386ca07ecc64960655c7e2
Sergey <xxsmotur@gmail.com>
2019-01-09 14:56:43 +0500

    Vector: implementation of Len method

--- linear/vector.go
+++ linear/vector.go
@@ -2,3 +2,13 @@ package linear
 
 // Vector provides definition of the vector
 type Vector []float64
+
+// Len returns size of vector
+func (v *Vector) Len() int {
+	return len(*v)
+}
+
+// Dot provides implementation of dot product
+func (v *Vector) Dot(p *Vector) *Vector {
+	return v
+}
