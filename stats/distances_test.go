8d145d27dcca94489a34046a905c1ee5fcbe2db0
Sergey <xxsmotur@gmail.com>
2019-01-20 22:38:48 +0500

    testing of EuclideanDistance

--- stats/distances_test.go
+++ stats/distances_test.go
@@ -5,5 +5,10 @@ import (
 )
 
 func TestEuclideanDistance(t *testing.T) {
-
+	x := []float64{1.1,2.2}
+	y := []float64{5.5,0.2}
+	_, err := EuclideanDistance(x, y)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
