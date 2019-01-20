f4264180db129620a2f9cbaa4d0332f80d48b1cb
Sergey <xxsmotur@gmail.com>
2019-01-20 18:33:18 +0500

    add validate distance input method

--- stats/distances.go
+++ stats/distances.go
@@ -1,13 +1,31 @@
 package stats
 
-import "math"
+import (
+	"errors"
+	"math"
+)
+
+var errLengthMismatch = errors.New("size of slices is not equal")
 
 // EuclideanDistance returns distance between two vectors
 // https://en.wikipedia.org/wiki/Euclidean_distance
 func EuclideanDistance(x, y []float64) (float64, error) {
+	if err := validateDistanceInput(x, y); err != nil {
+		return 0, err
+	}
 	var distance float64
 	for i := 0; i < len(x); i++ {
 		distance = distance + ((x[i] - y[i]) * (x[i] - y[i]))
 	}
 	return math.Sqrt(distance), nil
+}
+
+// validateDistanceInput returns nil if input for get distance
+// between two slices is ok
+func validateDistanceInput(x, y []float64) error {
+	if len(x) != len(y) {
+		return errLengthMismatch
+	}
+
+	return nil
 }
