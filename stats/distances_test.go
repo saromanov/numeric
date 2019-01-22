75759b1487b41ad2662128a7b3a2a64662fa3af0
Sergey <xxsmotur@gmail.com>
2019-01-22 11:49:46 +0500

    Add test for ManhattanDistance

--- stats/distances_test.go
+++ stats/distances_test.go
@@ -13,4 +13,23 @@ func TestEuclideanDistance(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, result, float64(4.833218389437829), "not expected output")
 
+	x = []float64{1.1}
+	y = []float64{5.5, 0.2}
+	_, err = EuclideanDistance(x, y)
+	assert.Error(t, err)
+
+}
+
+func TestManhattanDistance(t *testing.T) {
+	x := []float64{1.1, 2.2}
+	y := []float64{5.5, 0.2}
+	result, err := ManhattanDistance(x, y)
+	assert.NoError(t, err)
+	assert.Equal(t, result, float64(6.4), "not expected output")
+
+	x = []float64{1.1}
+	y = []float64{5.5, 0.2}
+	_, err = ManhattanDistance(x, y)
+	assert.Error(t, err)
+
 }
