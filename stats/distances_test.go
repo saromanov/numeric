602399d85e107419b547fee98b5d42ab7797cbf2
Sergey <xxsmotur@gmail.com>
2019-01-22 11:44:39 +0500

    Fixing of test

--- stats/distances_test.go
+++ stats/distances_test.go
@@ -2,14 +2,15 @@ package stats
 
 import (
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestEuclideanDistance(t *testing.T) {
-	x := []float64{1.1,2.2}
-	y := []float64{5.5,0.2}
+	x := []float64{1.1, 2.2}
+	y := []float64{5.5, 0.2}
 	result, err := EuclideanDistance(x, y)
 	assert.NoError(t, err)
-	assert.Equal(t, result, float64(10), "not expected output")
-	
+	assert.Equal(t, result, float64(4.833218389437829), "not expected output")
+
 }
