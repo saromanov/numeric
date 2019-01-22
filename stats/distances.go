afc17e72f6a07903c575e5bebf6270a67168f077
Sergey <xxsmotur@gmail.com>
2019-01-22 11:47:59 +0500

    implementation of ManhattanDistance

--- stats/distances.go
+++ stats/distances.go
@@ -20,6 +20,19 @@ func EuclideanDistance(x, y []float64) (float64, error) {
 	return math.Sqrt(distance), nil
 }
 
+// ManhattanDistance return distance between two vectors
+// https://en.wikipedia.org/wiki/Taxicab_geometry
+func ManhattanDistance(x, y []float64) (float64, error) {
+	if err := validateDistanceInput(x, y); err != nil {
+		return 0, err
+	}
+	var d float64
+	for i := 0; i < len(x); i++ {
+		d += math.Abs(x[i] - y[i])
+	}
+	return d, nil
+}
+
 // validateDistanceInput returns nil if input for get distance
 // between two slices is ok
 func validateDistanceInput(x, y []float64) error {
